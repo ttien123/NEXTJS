7344939290131e1e8c06156c1d98e06683d8e9f3
the tien <tien.nguyenthe@amela.vn>
2024-10-27 17:33:46 +0700

    code/bai23

--- src/app/(public)/(auth)/login/login-form.tsx
+++ src/app/(public)/(auth)/login/login-form.tsx
@@ -7,8 +7,14 @@ import { useForm } from 'react-hook-form'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { LoginBody, LoginBodyType } from '@/schemaValidations/auth.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useLoginMutation } from '@/queries/useAuth'
+import { Toast } from '@/components/ui/toast'
+import { toast } from '@/hooks/use-toast'
+import { handleErrorApi } from '@/lib/utils'
 
 export default function LoginForm() {
+  const loginMutation = useLoginMutation()
+
   const form = useForm<LoginBodyType>({
     resolver: zodResolver(LoginBody),
     defaultValues: {
@@ -16,6 +22,21 @@ export default function LoginForm() {
       password: ''
     }
   })
+
+  const onSubmit = async(data: LoginBodyType) => {
+    if (loginMutation.isPending) return
+    try {
+      const result = await loginMutation.mutateAsync(data)
+      toast({
+        description: result.payload.message
+      })
+    } catch (error: any) {
+      handleErrorApi({
+        error,
+        setError: form.setError
+      })
+    }
+  }
 
   return (
     <Card className='mx-auto max-w-sm'>
@@ -25,7 +46,9 @@ export default function LoginForm() {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form className='space-y-2 max-w-[600px] flex-shrink-0 w-full' noValidate>
+          <form onSubmit={form.handleSubmit(onSubmit, err => {
+            console.log(err);
+          })} className='space-y-2 max-w-[600px] flex-shrink-0 w-full' noValidate>
             <div className='grid gap-4'>
               <FormField
                 control={form.control}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { clsx, type ClassValue } from "clsx"
+import { UseFormSetError } from "react-hook-form"
 import { twMerge } from "tailwind-merge"
+import { EntityError } from "./http"
+import { toast } from "@/hooks/use-toast"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,4 +14,30 @@ export function cn(...inputs: ClassValue[]) {
  */
 export const normalizePath = (path: string) => {
   return path.startsWith('/') ? path.slice(1) : path
+}
+
+export const handleErrorApi = ({
+  error,
+  setError,
+  duration
+}: {
+  error: any
+  setError?: UseFormSetError<any>
+  duration?: number
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: 'server',
+        message: item.message
+      })
+    })
+  } else {
+    toast({
+      title: 'Lỗi',
+      description: error?.payload?.message ?? 'Lỗi không xác định',
+      variant: 'destructive',
+      duration: duration ?? 5000
+    })
+  }
 }
