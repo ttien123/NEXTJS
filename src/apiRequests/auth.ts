c86f75316dd99678cf40f5cd48f1dcfb28579086
the tien <tien.nguyenthe@amela.vn>
2024-11-03 22:46:10 +0700

    code/bai27

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,11 +1,27 @@
 import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+} from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
-    slogin: (body: LoginBodyType) => http.post<LoginResType>('/auth/login', body),
-    login: (body: LoginBodyType) => http.post<LoginResType>('api/auth/login', body, {
-        baseUrl: '',
-    })
-}
+  sLogin: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body),
+  login: (body: LoginBodyType) =>
+    http.post<LoginResType>("api/auth/login", body, {
+      baseUrl: "",
+    }),
+  sLogout: (body: LogoutBodyType & {
+    accessToken : string
+  }) => http.post("/auth/logout", {
+    refreshToken: body.refreshToken
+  }, {
+    headers: {
+        Authorization: `Bearer ${body.accessToken}`
+    }
+  }),
+  logout: () =>
+    http.post("/api/auth/logout", null, { baseUrl: '' }),
+};
 
-export default authApiRequest
+export default authApiRequest;
