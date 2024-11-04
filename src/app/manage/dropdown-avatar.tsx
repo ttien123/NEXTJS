050ffd330732f7a622ee34e6a36f316b8ba572b5
thetien <tien.nguyenthe@amela.vn>
2024-11-04 14:45:56 +0700

    code/bai29

--- src/app/manage/dropdown-avatar.tsx
+++ src/app/manage/dropdown-avatar.tsx
@@ -13,6 +13,7 @@ import Link from 'next/link'
 import { useLogoutMutation } from '@/queries/useAuth'
 import { handleErrorApi } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
+import { useAccountProfile } from '@/queries/useAccount'
 
 const account = {
   name: 'Nguyễn Văn A',
@@ -22,6 +23,8 @@ const account = {
 export default function DropdownAvatar() {
   const logoutMutation = useLogoutMutation()
   const router = useRouter()
+  const { data } = useAccountProfile()
+  const account = data?.payload.data
   const logout = async () => {
     if (logoutMutation.isPending) return
     try {
@@ -40,13 +43,13 @@ export default function DropdownAvatar() {
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon' className='overflow-hidden rounded-full'>
           <Avatar>
-            <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
-            <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+            <AvatarImage src={account?.avatar ?? undefined} alt={account?.name} />
+            <AvatarFallback>{account?.name.slice(0, 2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
+        <DropdownMenuLabel>{account?.name}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
           <Link href={'/manage/setting'} className='cursor-pointer'>
