19c3727f7598be38f382e7b6afab9e997f115662
thetien <tien.nguyenthe@amela.vn>
2024-11-12 17:16:06 +0700

    code/bai35-36

---
+++ src/app/(public)/(auth)/logout/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { getAccessTokenFromLS, getRefreshTokenFromLS } from '@/lib/utils';
+import { useLogoutMutation } from '@/queries/useAuth';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useRef } from 'react';
+
+
+const LogoutPage = () => {
+    const { mutateAsync } = useLogoutMutation()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const refreshTokenFromUrl = searchParams.get('refreshToken')
+    const accessTokenFromUrl = searchParams.get('accessToken')
+    const ref = useRef<any>(null)
+    useEffect(() => {
+        if (ref.current || 
+            (refreshTokenFromUrl && refreshTokenFromUrl !== getRefreshTokenFromLS()) || 
+            (accessTokenFromUrl && accessTokenFromUrl !== getAccessTokenFromLS())) {
+            return
+        }
+        ref.current = mutateAsync
+        mutateAsync().then(() => {
+            setTimeout(() => {
+                ref.current = null
+            })
+            router.push('/login')
+        })
+    }, [mutateAsync, router])
+    
+    return (
+        <div>
+            Logout....
+        </div>
+    );
+}
+
+export default LogoutPage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,30 @@ const unAuthPaths = ['/login']
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl
-    const isAuth = Boolean(request.cookies.get('accessToken')?.value)
-    if (privatePaths.some(path => pathname.startsWith(path) && !isAuth)) {
+    const accessToken = request.cookies.get('accessToken')?.value
+    const refreshToken = request.cookies.get('refreshToken')?.value
+
+    //Chưa đăng nhập thì không cho vào private paths
+    if (privatePaths.some(path => pathname.startsWith(path) && !refreshToken)) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
 
-    if (unAuthPaths.some(path => pathname.startsWith(path) && isAuth)) {
+    //Đang nhập rồi thì không cho vào login nữa
+    if (unAuthPaths.some(path => pathname.startsWith(path) && refreshToken)) {
         return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    //Trường hợp đăng nhập rồi, nhưng access token hết hạn
+
+    if (privatePaths.some(path => pathname.startsWith(path) && !accessToken && refreshToken)) {
+        const url = new URL('/logout', request.url)
+        url.searchParams.set('refreshToken', request.cookies.get('refreshToken')?.value ?? '')
+        return NextResponse.redirect(url)
     }
 
     return NextResponse.next()
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/manage/:path*', '/login'],
 }
