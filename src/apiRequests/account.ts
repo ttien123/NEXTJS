eecde07cee7807cc4c57203c9d5de4d319737b83
thetien <tien.nguyenthe@amela.vn>
2024-11-20 10:24:20 +0700

    code/bai47

--- src/apiRequests/account.ts
+++ src/apiRequests/account.ts
@@ -1,18 +1,25 @@
 import http from "@/lib/http";
-import { AccountResType, ChangePasswordBodyType, ChangePasswordV2BodyType, ChangePasswordV2ResType, UpdateMeBodyType } from "@/schemaValidations/account.schema";
+import { AccountListResType, AccountResType, ChangePasswordBodyType, ChangePasswordV2BodyType, ChangePasswordV2ResType, CreateEmployeeAccountBodyType, UpdateEmployeeAccountBodyType, UpdateMeBodyType } from "@/schemaValidations/account.schema";
+
+const prefix = '/accounts'
 
 const accountApiRequest = {
-    me: () => http.get<AccountResType>('/accounts/me'),
-    updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>('/accounts/me', body),
-    changePassword: (body: ChangePasswordBodyType) => http.put<AccountResType>('/accounts/change-password', body),
-    sChangePasswordV2: (accessToken: string, body: ChangePasswordV2BodyType) => http.put<ChangePasswordV2ResType>('/accounts/change-password-v2', body, {
+    me: () => http.get<AccountResType>(`${prefix}/me`),
+    updateMe: (body: UpdateMeBodyType) => http.put<AccountResType>(`${prefix}/me`, body),
+    changePassword: (body: ChangePasswordBodyType) => http.put<AccountResType>(`${prefix}/change-password`, body),
+    sChangePasswordV2: (accessToken: string, body: ChangePasswordV2BodyType) => http.put<ChangePasswordV2ResType>(`${prefix}/change-password-v2`, body, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         },
     }),
-    changePasswordV2: (body: ChangePasswordV2BodyType) => http.put<ChangePasswordV2ResType>('/api/accounts/change-password-v2', body, {
+    changePasswordV2: (body: ChangePasswordV2BodyType) => http.put<ChangePasswordV2ResType>(`/api${prefix}/change-password-v2`, body, {
         baseUrl: ''
     }),
+    list: () => http.get<AccountListResType>(`${prefix}`),
+    addEmployee: (body: CreateEmployeeAccountBodyType) => http.post<AccountResType>(prefix, body),
+    updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) => http.put<AccountResType>(`${prefix}/detail/${id}`, body),
+    getEmployee: (id: number) => http.get<AccountResType>(`${prefix}/detail/${id}`),
+    deleteEmployee: (id: number) => http.delete<AccountResType>(`${prefix}/detail/${id}`),
 }
 
 export default accountApiRequest
--- src/queries/useAccount.tsx
+++ src/queries/useAccount.tsx
@@ -1,5 +1,6 @@
 import accountApiRequest from "@/apiRequests/account"
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 export const useAccountMe = () => {
     return useQuery({
@@ -17,5 +18,55 @@ export const useUpdateMeMutation = () => {
 export const useChangePasswordMutation = () => {
     return useMutation({
         mutationFn: accountApiRequest.changePasswordV2,
+    })
+}
+
+export const useGetAccountList = () => {
+    return useQuery({
+        queryKey: ['accounts'],
+        queryFn: accountApiRequest.list
+    })
+}
+
+export const useGetAccount = ({id}: {id: number}) => {
+    return useQuery({
+        queryKey: ['accounts', id],
+        queryFn: () => accountApiRequest.getEmployee(id)
+    })
+}
+
+export const useAddAccountMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: accountApiRequest.addEmployee,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        }
+    })
+}
+
+export const useUpdateAccountMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: ({id, ...body}: { id: number} & UpdateEmployeeAccountBodyType) => accountApiRequest.updateEmployee(id, body),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        }
+    })
+}
+
+export const useDeleteAccountMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: accountApiRequest.deleteEmployee,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['accounts']
+            })
+        }
     })
 }
