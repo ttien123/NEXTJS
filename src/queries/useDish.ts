dde9ad743070d5a4ba0b857695290ccd7a6b5145
thetien <tien.nguyenthe@amela.vn>
2024-11-21 11:53:21 +0700

    code/bai54

---
+++ src/queries/useDish.ts
@@ -0,0 +1,57 @@
+import accountApiRequest from "@/apiRequests/account"
+import dishApiRequest from "@/apiRequests/dist"
+import { UpdateEmployeeAccountBodyType } from "@/schemaValidations/account.schema"
+import { UpdateDishBodyType } from "@/schemaValidations/dish.schema"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+
+export const usDistListQuery = () => {
+    return useQuery({
+        queryKey: ['dishes'],
+        queryFn: dishApiRequest.list
+    })
+}
+
+export const useGetDishQuery = ({id, enabled}: {id: number, enabled?: boolean}) => {
+    return useQuery({
+        queryKey: ['dishes', id],
+        queryFn: () => dishApiRequest.getDish(id),
+        enabled
+    })
+}
+
+export const useAddDishMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: dishApiRequest.add,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['dishes']
+            })
+        }
+    })
+}
+
+export const useUpdateDishMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: ({id, ...body}: { id: number} & UpdateDishBodyType) => dishApiRequest.updateDish(id, body),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['dishes'],
+                exact: true
+            })
+        }
+    })
+}
+
+export const useDeleteAccountMutation = () => {
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn: accountApiRequest.deleteEmployee,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['dishes']
+            })
+        }
+    })
+}
