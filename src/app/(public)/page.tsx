eee557de4ca11dbb6fd40f1bfb35059959c85ef2
thetien <tien.nguyenthe@amela.vn>
2024-11-22 16:44:28 +0700

    code/bai66

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,19 @@
+import dishApiRequest from '@/apiRequests/dish'
+import { DishListResType } from '@/schemaValidations/dish.schema'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  let dishList: DishListResType['data'] = []
+  try {
+    const result = await dishApiRequest.list()
+    const { payload: { data } } = result
+    dishList = data
+  } catch (error) {
+    return <div>Something went wrong</div>
+  }
   return (
     <div className='w-full space-y-4'>
-      <div className='relative'>
+      <section className='relative'>
         <span className='absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10'></span>
         <Image
           src='/banner.png'
@@ -17,24 +27,25 @@ export default function Home() {
           <h1 className='text-center text-xl sm:text-2xl md:text-4xl lg:text-5xl font-bold'>Nhà hàng Big Boy</h1>
           <p className='text-center text-sm sm:text-base mt-4'>Vị ngon, trọn khoảnh khắc</p>
         </div>
-      </div>
+      </section>
       <section className='space-y-10 py-16'>
         <h2 className='text-center text-2xl font-bold'>Đa dạng các món ăn</h2>
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-10'>
-          {Array(4)
-            .fill(0)
-            .map((_, index) => (
-              <div className='flex gap-4 w' key={index}>
+          {dishList.map((dish) => (
+              <div className='flex gap-4 w' key={dish.id}>
                 <div className='flex-shrink-0'>
-                  <img
-                    src='https://ik.imagekit.io/freeflo/production/6b91c700-92c4-4601-8e96-37d84ac3c28c.png?tr=w-2048,q-75&alt=media&pr-true'
+                  <Image
+                    width={150}
+                    height={150}
+                    alt={dish.name}
+                    src={dish.image}
                     className='object-cover w-[150px] h-[150px] rounded-md'
                   />
                 </div>
                 <div className='space-y-1'>
-                  <h3 className='text-xl font-semibold'>Bánh mì</h3>
-                  <p className=''>Bánh mì sandwidch</p>
-                  <p className='font-semibold'>123,123đ</p>
+                  <h3 className='text-xl font-semibold'>{dish.name}</h3>
+                  <p className=''>{dish.description}</p>
+                  <p className='font-semibold'>{dish.price}</p>
                 </div>
               </div>
             ))}
