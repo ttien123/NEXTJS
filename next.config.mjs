1fc32a9beec9fcf173885e50d648c22a18aa1162
thetien <tien.nguyenthe@amela.vn>
2024-11-27 13:55:10 +0700

    code/bai75

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
             port: '4000',
             pathname: '/**',
           },
+          {
+            hostname: 'via.placeholder.com',
+            pathname: '/**',
+          }
         ],
       },
 };
